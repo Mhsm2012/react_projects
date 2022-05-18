1665b011c20e2f4279ab228f5f84f4af6bbf8cd5
mohamed <eliwa554@gmail.com>
2022-05-19 02:09:17 +0300

    chnages

---
+++ tours/src/App.js
@@ -0,0 +1,11 @@
+import React,{useState,useEffect} from 'react';
+import './App.css';
+
+function App() {
+  const [loading,setLoading] = useState(true)
+  const[tours,setTours] = useState([])
+  return (
+    
+}
+
+export default App;
