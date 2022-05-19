75a30c402691a152e07697e0822dfe27239733d0
mohamed <eliwa554@gmail.com>
2022-05-19 21:30:44 +0300

    build and remove

---
+++ banking_app/src/App.js
@@ -0,0 +1,12 @@
+
+import 'bulma'
+
+function App() {
+  return (
+    <div  className='section'>
+    <h1 className='title has-text-centered is-size-1'> banking app</h1>      
+    </div>
+  );
+}
+
+export default App;
