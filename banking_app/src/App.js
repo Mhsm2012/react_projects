5b61c265ed1859f3407962ae496ff54a9d587cf4
mohamed <eliwa554@gmail.com>
2022-05-20 02:12:08 +0300

    m

--- banking_app/src/App.js
+++ banking_app/src/App.js
@@ -2,12 +2,16 @@
 import 'bulma'
 import Header from './Header';
 import Banking from './Banking'
+import Balance from './Balance'
+import AccountType from './AccountType'
 
 function App() {
   return (
     <div  className='section'>
    <Header />   
+   <Balance />
    <Banking /> 
+   <AccountType />
     </div>
   );
 }
---
+++ banking_app/src/reducers/bankingReducer.js
@@ -0,0 +1,14 @@
+
+const initialState = {
+    balance :0,
+    interset:0,
+}
+
+
+ export const bankingReducer = (state,action) =>{
+    switch(action.type){
+        default:
+            return state
+       
+    }
+}
