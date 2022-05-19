9a8c9c1c0c6645387b1bbb74510e90384515b56c
mohamed <eliwa554@gmail.com>
2022-05-19 05:05:16 +0300

    changes

---
+++ tours/src/Loading.js
@@ -0,0 +1,13 @@
+// loading page that load when app is still loading 
+
+import React from "react";
+const Loading = () =>{
+
+
+    return(
+        <div className="loading">
+            <h1>loading ....</h1>
+        </div>
+    );
+};
+export default Loading;
