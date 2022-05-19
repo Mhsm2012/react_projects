6f44b24611273875ee2f95d22ed3608097e04124
mohamed <eliwa554@gmail.com>
2022-05-20 02:11:56 +0300

    m

---
+++ banking_app/src/store.js
@@ -0,0 +1,4 @@
+import {createStore,applyMiddleware} from 'redux'
+import { bankingReducer } from './reducers/bankingReducer'
+import {composeWithDevTools} from 'redux-devtools-extension'
+ export const store = createStore(bankingReducer,composeWithDevTools(applyMiddleware()))
