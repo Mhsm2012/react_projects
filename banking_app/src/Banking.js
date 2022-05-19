d34ec50afd561ebe4b888083c5607eb5efcc7d21
mohamed <eliwa554@gmail.com>
2022-05-20 02:50:04 +0300

    changes

--- banking_app/src/Banking.js
+++ banking_app/src/Banking.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Banking() {
+  const[amount,setAmount] = useState(0)
+  const handleChange =(e)=>{
+    setAmount(e.target.value)
+  }
   return (
     <div className='field'>
-        <input type="number" className='input' placeholder='amount' />
+        <input type="number" className='input' placeholder='amount' value={amount} onChange={handleChange}/>
         <button className='button  mr-2 is-success'>Deposit</button>
         <button className='button  mr-2 is-danger'>Withdraw</button>
         <button className='button  mr-2 is-link'>Collect Interset</button>
