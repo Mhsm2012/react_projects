4d4fc2895301a8a91c99a541e6ecb63e7da7134d
mohamed <eliwa554@gmail.com>
2022-05-22 20:45:42 +0300

    changes

--- banking_app/src/Banking.js
+++ banking_app/src/Banking.js
@@ -1,24 +1,24 @@
 import React, { useState } from 'react'
 import {useDispatch} from 'react-redux'
-import { deposit } from './actions'
+import { deposit ,withDraw} from './actions'
 
-function Banking() {
-  const[amount,setAmount] = useState(0)
+const  Banking=( )=> {
+  const[ amount,setAmount] = useState(0)
 
   const dispatch = useDispatch()
 
-  const handleChange =(e)=>{
+  const handleChange = e =>{
     setAmount(e.target.value)
   }
 
-  const handleDeposit =() =>{
-    dispatch(deposit(amount))
+  const handleDeposit =() => {
+    dispatch(deposit(+amount))
 
   }
   return (
     <div className='field'>
         <input type="number" className='input' placeholder='amount' value={amount} onChange={handleChange}/>
-        <button className='button  mr-2 is-success'>Deposit</button>
+        <button className='button  mr-2 is-success'  onClick={handleDeposit}>Deposit</button>
         <button className='button  mr-2 is-danger'>Withdraw</button>
         <button className='button  mr-2 is-link'>Collect Interset</button>
         <button className='button  mr-2 is-info'>Delete Account</button>
